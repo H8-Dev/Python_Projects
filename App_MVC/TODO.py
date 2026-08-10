c14ecab0a1144db3913502896c715165dc22719a
H8-Dev <h8.pr0j1@gmail.com>
2026-08-10 18:21:15 -0300

    Add project structure outline in TODO.py

---
+++ App_MVC/TODO.py
@@ -0,0 +1,21 @@
+#O projeto deverá utilizar, no mínimo, a seguinte organização:
+#    helpdesk/
+ 
+#  ├── controllers/ 
+#  │         ├── usuario_controller.py 
+#  │         └── chamado_controller.py 
+#  │ 
+#  ├── services/ 
+#  │        ├── usuario_service.py 
+#  │        └── chamado_service.py 
+#  │
+#  ├── repositories/ 
+#  │         ├── usuario_repository.py 
+#  │         └── chamado_repository.py
+#  │ 
+#  ├── models/ 
+#  │         ├── usuario.py 
+#  │         └── chamado.py 
+#  │
+#  ├── database.py
+#  └── app.py      
